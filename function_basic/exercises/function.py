8082e371a8d9c5be54d3790d4cbed1de3ca6c6f3
DecerZz <ravel.dello007@gmail.com>
2023-10-30 11:02:11 +0200

    functions ja tingimusolendid

---
+++ function_basic/exercises/function.py
@@ -0,0 +1,26 @@
+"""Function examples."""
+
+
+# func()
+def func():
+    print("I´m inside the function")
+
+
+# my_name_is(name)
+def my_name_is(name: str):
+    print(f"My name is {name}")
+
+
+# sum_six(num)`
+def sum_six(num: int): -> int
+    return num + 6
+
+
+# sum_numbers()
+def sum_numbers(a: int, b: int): -> int
+    return a + b
+
+
+# usd_to_eur()
+def usd_to_eur(a: int): -> int
+    return a * 0.8
